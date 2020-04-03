41bc0f61bfa167b243a79aef04837d74ec99c39f
Tyler Scott <tyduptyler13@gmail.com>
2020-04-02 18:04:12 -0600

    Checking in docker work (minus java repo)

--- docker/fusionauth/test/fusionauth-app/build.sh
+++ docker/fusionauth/test/fusionauth-app/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd ~/dev/inversoft/fusionauth/fusionauth-app
+cd ~/dev/inversoft/fusionauth/fusionauth-app || exit 1
 sb bundle --zipOnly
 cd - || exit 1
-cp ~/dev/inversoft/fusionauth/fusionauth-app/build/bundles/fusionauth-app-*.zip fusionauth-app.zip
+rm -rf ./fusionauth-app
+unzip ~/dev/inversoft/fusionauth/fusionauth-app/build/bundles/fusionauth-app-*.zip -d ./fusionauth-app
 docker build -t fusionauth/fusionauth-app:dev .
